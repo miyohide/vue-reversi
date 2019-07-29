d0ac3093110405a7db985a6d237016e74e39b3dc
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-29 19:18:59 +0900

    石の色を変化させるようにした

---
+++ src/components/Piece.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="piece" :class="{ white, black }"></div>
+</template>
+
+<script>
+import { WHITE, BLACK } from '@/store';
+
+export default {
+    props: {
+        color: {
+            required: true,
+            type: String,
+        },
+    },
+    computed: {
+        white () {
+            return this.color === WHITE;
+        },
+        black() {
+            return this.color === BLACK;
+        }
+    }
+}
+</script>
