bad7be8e30760f94d5c22e799a300ffe63845643
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-30 22:00:19 +0900

    implement Board component.

---
+++ src/components/Board.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="board-container">
+        <div
+            v-for="cell in cells"
+            class="cell"
+            :key="cell.key">
+            <piece
+                v-if="cell.value !== 'E'"
+                :color="cell.value" />
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import Piece from '@/components/Piece.vue';
+
+export default {
+    components: {
+        Piece,
+    },
+    computed: {
+        ...mapState([
+            'board',
+        ]),
+        cells() {
+            return this.board.map((cell, index) => ({ key: `cell-${index}`, value: cell }));
+        },
+    },
+};
+</script>
