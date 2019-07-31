ad7261d5be120d3ad19d55e95a679e3fee95d404
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-31 20:32:39 +0900

    add board style.

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -29,3 +29,31 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.board-container {
+  width: 50vw;
+  height: 50vw;
+  border: 12px solid #6D5720;
+  border-radius: 12px;
+  margin: 0 auto;
+  min-width: 300px;
+  min-height: 300px;
+  max-width: 700px;
+  max-height: 700px;
+}
+ .board {
+  display: grid;
+  grid-template-columns: 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5%;
+  grid-template-rows: 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5% 12.5%;
+  width: 100%;
+  height: 100%;
+  background: #6C9A33;
+}
+.cell {
+  width: 100%;
+  height: 100%;
+  box-shadow: inset 1px 1px 0 #94C160, inset -1px -1px 0 #477413;
+  display: flex;
+}
+</style>
