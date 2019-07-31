e9766b645c0513e98b03f57c9c08aef61e8a0097
Hidenori Miyoshi <miyohide@gmail.com>
2019-07-31 20:34:34 +0900

    add coordinate/cellIndex spec.

--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -20,4 +20,22 @@ describe('store', () => {
             expect(state.currentPlayer).toEqual(BLACK);
         });
     });
+
+    describe('tools', () => {
+        describe('coordinates', () => {
+            it('配列のindex値をx,y座標に正しく変換すること', () => {
+                expect(coordinates(0)).toEqual([0, 0]);
+                expect(coordinates(20)).toEqual([4, 2]);
+                expect(coordinates(15)).toEqual([7, 1]);
+            });
+        });
+
+        describe('cellIndex', () => {
+            it('x,y座標を配列のindex値に正しく変換すること', () => {
+                expect(cellIndex(0, 0)).toEqual(0);
+                expect(cellIndex(4, 2)).toEqual(20);
+                expect(cellIndex(7, 1)).toEqual(15);
+            });
+        });
+    });
 });
