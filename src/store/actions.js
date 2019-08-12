cecfeaffa6910b6f9f5e05ff7741c2c37f42c8fc
Hidenori Miyoshi <miyohide@gmail.com>
2019-08-12 12:19:43 +0900

    石を置いたことを実装

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,7 @@
 export default {
     play(context, position) {
         const { currentPlayer } = context.state;
+        context.commit('changePiece', { position: position, color: currentPlayer});
         context.commit('changePlayer');
     },
 };
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -6,4 +6,7 @@ export default {
     changePlayer(state) {
         state.currentPlayer = opponent(state.currentPlayer); 
     },
+    changePiece(state, { position, color }) {
+        state.board[position] = color;
+    },
 };
