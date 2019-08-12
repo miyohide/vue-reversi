5ea0f095d6644e88201dd624a4ccf9f8c3e0a1eb
Hidenori Miyoshi <miyohide@gmail.com>
2019-08-12 15:39:42 +0900

    reversibleCellsのテストを追加

--- tests/unit/store/getters.spec.js
+++ tests/unit/store/getters.spec.js
@@ -8,4 +8,12 @@ describe('store::getters', () => {
             expect(result).toEqual([20, 29, 34, 43]);
         });
     });
+
+    describe('reversibleCells', () => {
+        it('反転できるセルが返ること', () => {
+            const result = getters.reversibleCells(state)(20);
+            result.sort();
+            expect(result).toEqual([20, 28]);
+        });
+    });
 });
