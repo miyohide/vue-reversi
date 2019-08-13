4266d4e7036249bf39df522b758d0eb9a448396d
Hidenori Miyoshi <miyohide@gmail.com>
2019-08-13 13:34:42 +0900

    create Dashboard spec.

---
+++ tests/unit/dashboard.spec.js
@@ -0,0 +1,21 @@
+import { mount } from '@vue/test-utils';
+import { createStore } from '@/store';
+import Dashboard from '@/components/Dashboard.vue';
+
+describe('components::Dashboard', () => {
+    let wrapper;
+    let store;
+
+    beforeEach(() => {
+        store = createStore();
+        wrapper = mount(Dashboard, {
+            store,
+        });
+    });
+
+    describe('render scores', () => {
+        it('プレイヤーのスコアが描かれること', () => {
+            expect(wrapper.findAll('.score').length).toEqual(1);
+        });
+    });
+});
