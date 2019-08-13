661b7deb1ff6a8dd830faebfeea88d61336894ee
Hidenori Miyoshi <miyohide@gmail.com>
2019-08-13 14:58:30 +0900

    display score.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,6 +2,27 @@
     <div class="dashboard">
         <div class="score">
             <h2>Scores</h2>
+            <div class="scores-container">
+                <div>
+                    Black: {{blackPlayerScore}}
+                </div>
+                <div>
+                    White: {{whitePlayerScore}}
+                </div>
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+    computed: {
+        ...mapGetters([
+            'blackPlayerScore',
+            'whitePlayerScore',
+        ]),
+    },
+};
+</script>
